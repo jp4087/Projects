e0ba761df1b1767bd6df031ac636985f79bf303f
jp4087 <67579506+jp4087@users.noreply.github.com>
2021-08-15 15:52:56 +0530

    Add files via upload

---
+++ React/resume-builder/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducer/rootReducer';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import { composeWithDevTools} from 'redux-devtools-extension'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBnUUST383caVs-3N3oLWHBbCFzRQzV4Z8",
+  authDomain: "resume-builder-fc36c.firebaseapp.com",
+  projectId: "resume-builder-fc36c",
+  storageBucket: "resume-builder-fc36c.appspot.com",
+  messagingSenderId: "961725033564",
+  appId: "1:961725033564:web:0c84af7e199cf716ebca99"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore()
+
+const reduxStore = createStore(rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})), 
+    reduxFirestore(firebase) // redux bindings for firestore,  
+  )
+);
+
+
+ReactDOM.render(
+  <Provider store={reduxStore}>
+    <BrowserRouter>
+    <ReactReduxFirebaseProvider
+      firebase={firebase}
+      config={firebaseConfig}
+      dispatch={reduxStore.dispatch}
+      createFirestoreInstance={createFirestoreInstance}
+      >
+      <App />
+    </ReactReduxFirebaseProvider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
